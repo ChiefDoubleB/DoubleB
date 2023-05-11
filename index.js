bb3889995f8cd599aece1e95a50d259cf810f2d5
ChiefDoubleB <98066762+ChiefDoubleB@users.noreply.github.com>
2023-05-10 21:07:28 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Hey! Thanks for stopping by!");
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+
+var randomImageSource = "dice-pictures/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0]; 
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "dice-pictures/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
