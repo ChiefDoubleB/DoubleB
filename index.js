eeb7de1babd3e9bb8b21153e0930bab8dd4eda58
ChiefDoubleB <98066762+ChiefDoubleB@users.noreply.github.com>
2023-05-11 20:02:58 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
 
 var randomImageSource = "dice-pictures/" + randomDiceImage;
 
-var image1 = document.querySelectorAll("img")[0]; 
+var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
@@ -17,11 +17,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //Player 1 victory
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-  }
-  else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-  }
-  else {
-    document.querySelector("h1").innerHTML = "Tie!";
-  }
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Tie!";
+}
